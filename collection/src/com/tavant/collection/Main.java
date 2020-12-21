34daac892d5b0b3016ff0a2b0be8f90f1a412b35
Virendra patel <patelvirendra62@gmail.com>
2020-12-21 08:52:04 +0530

    csv

--- collection/src/com/tavant/collection/Main.java
+++ collection/src/com/tavant/collection/Main.java
@@ -1,7 +1,20 @@
 package com.tavant.collection;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 import java.util.Random;
 import java.util.Scanner;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 
 import com.tavant.collection.exceptions.InvalidNameException;
 import com.tavant.collection.exceptions.InvalidSalaryException;
@@ -9,12 +22,75 @@ import com.tavant.collection.models.Employee;
 import com.tavant.collection.service.EmployeeService;
 import com.tavant.collection.service.EmployeeServiceImpl;
 
+
 public class Main {
 	private static EmployeeService employeeService = new EmployeeServiceImpl();
 	private static Scanner scanner = new Scanner(System.in);
 	private static boolean again = false;
 
-	public static void add() throws InvalidSalaryException , InvalidNameException{
+	public static void main(String[] args) throws Exception {
+//		customInput();
+		randomInput(100);
+		
+		
+
+		Optional<List<Employee>> emOptional = employeeService.getEmployees();
+		if(emOptional.isPresent()) {
+//			generating for all
+			generateCSVForAll(emOptional.get());
+			
+			emOptional.get().forEach(t -> {
+				try {
+//					generating for single user
+					generateCSVForSingleUser(t);
+				} catch (Exception c) {
+					c.printStackTrace();
+				}
+			});
+			
+			System.out.println("DOne...");
+		}
+	}
+	
+	public static void generateCSVForSingleUser(Employee employee) throws Exception {
+		String filename = "reports/"+employee.getEmployeeId()+"-" 
+	+ LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MMM-YYYY-hhmm"))+".csv";
+		
+		BufferedWriter writer = new BufferedWriter(new FileWriter(new File(filename)));
+		CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT);
+		
+		csvPrinter.printRecord(employee.getEmployeeId(), employee.getEmployeeFirstName(),
+				employee.getEmployeeLastName(), employee.getEmployeeMobileNumber(),
+				employee.getEmployeeSalary());
+		
+		writer.close();
+		csvPrinter.close();
+		
+		
+	}
+
+	public static void generateCSVForAll(List<Employee> employees) throws Exception {
+		String filename = "reports/EMP-Report" + 
+	LocalDate.now().format(DateTimeFormatter.ofPattern("dd-MM-YYYY"))+".csv";
+		BufferedWriter writer = new BufferedWriter(new FileWriter(new File(filename)));
+		CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT);
+
+		employees.forEach(employee -> {
+			try {
+				csvPrinter.printRecord(employee.getEmployeeId(), employee.getEmployeeFirstName(),
+						employee.getEmployeeLastName(), employee.getEmployeeMobileNumber(),
+						employee.getEmployeeSalary());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		});
+		writer.close();
+		csvPrinter.close();
+		System.out.println("CSV File Generated with file name : " + filename);
+	}
+
+	public static void add() throws InvalidSalaryException, InvalidNameException {
 		Employee employee = new Employee();
 
 		System.out.println("Enter Employee ID");
@@ -52,6 +128,13 @@ public class Main {
 		for (Employee employee3 : employeeService.getEmployees().get()) {
 			System.out.println(employee3);
 		}
+		
+//		generating CSV
+		try {
+			generateCSVForAll(employeeService.getEmployees().get());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 	}
 
@@ -65,35 +148,15 @@ public class Main {
 		}
 	}
 
+	private static void randomInput(int count) throws Exception {
 
-	public static void main(String[] args) throws Exception {
-		
-		
+		for (int i = 0; i < count; i++) {
+			String id = "AB" + new Random().nextInt(1000) + "" + new Random().nextInt(1000) + ""+ new Random().nextInt(1000);
+			employeeService.addEmployee(new Employee(id, "Name", "lastname",
+					"91444609" + new Random().nextInt(100), new Random().nextFloat() * 1000000));
 
-	    
-	   
-		customInput();
-//		randomInput();
-
-	}
-
-	private static  void randomInput() throws Exception {
-		
-		for (int i = 0; i < 10; i++) {
-			employeeService.addEmployee(new Employee(
-						"AB"+new Random().nextInt(20)+"", 
-						"Name" + i ,
-						"lastname" + i ,
-						"mobile" + i ,
-						new Random().nextFloat()
-						)
-					);
-					
 		}
-		
-		for (Employee iterable: employeeService.getEmployees().get()) {
-			System.out.println(iterable);
-		}
+
 	}
 
 	public static void customInput() throws InvalidSalaryException {
@@ -109,7 +172,7 @@ public class Main {
 					add();
 				} catch (InvalidNameException e) {
 					System.err.println(e);
-				}catch (InvalidSalaryException e) {
+				} catch (InvalidSalaryException e) {
 					System.err.println(e);
 				}
 				break;
