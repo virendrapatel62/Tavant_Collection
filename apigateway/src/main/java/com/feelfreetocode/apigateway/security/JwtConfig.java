05c3fa48a6c978fbb80b15174eb42b696430ce2e
virendra <virendra@Virendra>
2021-01-28 22:35:22 +0530

    updated

---
+++ apigateway/src/main/java/com/feelfreetocode/apigateway/security/JwtConfig.java
@@ -0,0 +1,22 @@
+package com.feelfreetocode.apigateway.security;
+
+import org.springframework.beans.factory.annotation.Value;
+
+
+public class JwtConfig {
+	@Value("${security.jwt.uri:/auth/**}")
+	private String Uri;
+
+	@Value("${security.jwt.header:Authorization}")
+	private String header;
+
+	@Value("${security.jwt.prefix:Bearer }")
+	private String prefix;
+
+	@Value("${security.jwt.expiration:#{24*60*60}}")
+	private int expiration;
+
+	@Value("${security.jwt.secret:soen6841}")
+	private String secret;
+
+}
